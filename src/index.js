853c6743888b60e64d03add6960c4c41f12bb926
phucxp12vn <vphuc8230tth@gmail.com>
2024-12-26 20:23:58 +0700

    implement load env variables

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const app = require("./config/express");
+require('dotenv-safe').config();
 
-const port = 3000;
+const port = process.env.PORT;
 app.listen(port, () => console.log(`server started on port ${port}`))
 
 module.exports = app; // don't know why
